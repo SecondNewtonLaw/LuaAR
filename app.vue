b065cdd1faabadcef0b9ea5974184c63a687dff7
Scriptone <arne.haers@student.odisee.be>
2024-12-15 13:11:48 +0100

    feat: add updater and process plugins, enhance permissions in Tauri configuration

--- app.vue
+++ app.vue
@@ -6,7 +6,7 @@
 </template>
 
 <script setup lang="ts">
-import { VSonner } from "vuetify-sonner"
+import { toast, VSonner } from "vuetify-sonner"
 import "vuetify-sonner/style.css"
 const reviewStore = useReviewStore()
 
@@ -19,7 +19,37 @@ useHead({
 
 onMounted(reviewStore.loadReviews)
 
-await callOnce(async () => {
-	console.log("Hello from the server!")
-})
+import { relaunch } from "@tauri-apps/plugin-process"
+import { check } from "@tauri-apps/plugin-updater"
+
+try {
+	const update = await check()
+	if (update) {
+		console.log(`found update ${update.version} from ${update.date} with notes ${update.body}`)
+		let downloaded = 0
+		let contentLength = 0
+		// alternatively we could also call update.download() and update.install() separately
+		await update.downloadAndInstall((event) => {
+			switch (event.event) {
+				case "Started":
+					contentLength = event.data.contentLength || 0
+					console.log(`started downloading ${event.data.contentLength} bytes`)
+					break
+				case "Progress":
+					downloaded += event.data.chunkLength || 0
+					console.log(`downloaded ${downloaded} from ${contentLength}`)
+					break
+				case "Finished":
+					console.log("download finished")
+					break
+			}
+		})
+
+		console.log("update installed")
+		await relaunch()
+	}
+} catch (error) {
+	console.log("failed to check for updates", error)
+	toast.error("Failed to check for updates")
+}
 </script>
