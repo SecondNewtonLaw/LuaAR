5492c568650f9c7537502ef6a10e16aaca165d2f
Scriptone <arne.haers@student.odisee.be>
2024-12-19 15:14:17 +0100

    feat: enhance update process with download progress indication and improved error handling

--- components/AppHeader.vue
+++ components/AppHeader.vue
@@ -13,13 +13,23 @@
 				<!-- <v-btn icon="mdi-file-document-outline" :to="{ name: 'HD' }" /> -->
 				<v-btn icon="mdi-shield-account-outline" :to="{ name: 'moderation' }" />
 				<v-btn icon="mdi-cog" @click="openSettings = true" />
+
 				<v-tooltip location="bottom">
 					<template #activator="{ props }">
 						<v-btn
 							v-bind="props"
-							icon="mdi-cloud-download-outline"
+							:icon="download === 100 ? 'mdi-check' : 'mdi-cloud-download-outline'"
+							:color="download === 100 ? 'success' : 'primary'"
 							@click="updateApplication"
-							v-if="update" />
+							v-if="(update && download === null) || download === 100" />
+
+						<v-progress-circular
+							v-bind="props"
+							class="mx-2 align-self-center"
+							:indeterminate="download === 0"
+							v-else-if="download !== null"
+							:model-value="download"
+							size="24" />
 					</template>
 					Update Available {{ update?.version }}
 				</v-tooltip>
@@ -36,50 +46,54 @@ import { check, Update } from "@tauri-apps/plugin-updater"
 import { toast } from "vuetify-sonner"
 
 const openSettings = ref(false)
-const update = ref<Update | null>(null)
-try {
-	update.value = await check()
-	console.log("update", update.value)
-	if (update.value) {
-		console.log(`found update ${update.value.version} from ${update.value.date} with notes ${update.value.body}`)
-	}
-} catch (error) {
-	console.log("failed to check for updates", error)
-	toast.error("Failed to check for updates")
-}
+let update: Update | null = null
+const download = ref<number | null>(null)
 
 const updateApplication = async () => {
-	if (!update.value) {
+	if (!update) {
 		console.log("no update available")
 		return
 	}
 	try {
 		let downloaded = 0
 		let contentLength = 0
-		// alternatively we could also call update.download() and update.install() separately
-		await update.value.downloadAndInstall((event) => {
+
+		await update.download((event) => {
+			console.log("downloaded", download.value)
 			switch (event.event) {
 				case "Started":
 					contentLength = event.data.contentLength || 0
-					console.log(`started downloading ${event.data.contentLength} bytes`)
+					console.log("download started", contentLength)
+					download.value = 0
 					break
 				case "Progress":
 					downloaded += event.data.chunkLength || 0
-					console.log(`downloaded ${downloaded} from ${contentLength}`)
+					download.value = (downloaded / contentLength) * 100
 					break
 				case "Finished":
 					console.log("download finished")
+					download.value = 100
 					break
 			}
 		})
 
-		console.log("update installed")
+		await new Promise((resolve) => setTimeout(resolve, 1000))
+		await update.install()
 		await relaunch()
 	} catch (error) {
 		console.log("failed to update", error)
 		toast.error("Failed to update")
 	}
 }
+
+onMounted(async () => {
+	try {
+		update = await check()
+	} catch (error) {
+		console.log("failed to check for updates", error)
+		toast.error("Failed to check for updates")
+	}
+})
 </script>
 
 <style scoped></style>
